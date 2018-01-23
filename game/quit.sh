e93b9a8cf4ae411102a97ad8526e9b20df3943a7
Meng Ran <mengran3275188@163.com>
2018-01-23 17:10:12 +0800

    完善停服脚本

--- game/quit.sh
+++ game/quit.sh
@@ -2,19 +2,28 @@ cd bin
 
 echo quit lobby
 ./AdminTool ../AdminTool/quitLobby.scp
-
 echo wait lobby last save
-
-echo quit battle
+./AdminTool ../AdminTool/waitLobby.scp
 
 echo quit database
 ./AdminTool ../AdminTool/quitDatabase.scp
+echo wait database last save
+./AdminTool ../AdminTool/waitDatabase.scp
+
 echo quit bwLobby
 ./AdminTool ../AdminTool/quitBwLobby.scp
 echo wait bwLobby last save
+./AdminTool ../AdminTool/quitBwLobby.scp
 
 echo quit bwDatabase
 ./AdminTool ../AdminTool/quitBwDatabase.scp
+echo wait bwDatabase last save
+./AdminTool ../AdminTool/waitBwDatabase.scp
+
 echo quit center
 ./AdminTool ../AdminTool/quitCenter.scp
 echo quit bwcenter
+./AdminTool ../AdminTool/quitBwCenter.scp
+
+echo quit battle
+pkill mono
